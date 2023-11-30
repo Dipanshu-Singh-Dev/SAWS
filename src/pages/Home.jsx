3e30514896272f9fcf2c06e18239dcb42d213f00
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.comnode>
2023-12-01 02:53:02 +0530

    added : fetching data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,20 @@ import Separator from "../components/Separator"
 import Heading from '../components/Heading';
 import Footer from '../components/Footer';
 import PreRegister from "../components/PreRegister";
-
+import axios from 'axios'
+import { useEffect, useState } from "react";
 const Home = () => {
-  
+  const url = "https://saws-backend.onrender.com/products";
+  const [headphones,setHeadphones] = useState();
+  const [phones, setPhones] = useState();
+  const [laptops, setLaptops] = useState();
+  const [tvs, setTvs] = useState();
+  useEffect(()=>{
+    axios.get(url + "?category=Headphone").then(res=>setHeadphones(res.data));
+    axios.get(url + "?category=Smartphone").then((res) => setPhones(res.data));
+    axios.get(url + "?category=Laptop").then((res) => setLaptops(res.data));
+    axios.get(url + "?category=TV").then((res) => setTvs(res.data));
+  },[])
   return (
     <div className={styles.container}>
       <img className={styles.img} src="/banner.jpg" alt="banner" />
@@ -14,16 +25,16 @@ const Home = () => {
       <PreRegister />
       <Separator />
       <Heading body={"Headphones"} />
-      <Carousel data={headphones} />
+      {headphones && <Carousel data={headphones} />}
       <Separator />
       <Heading body={"Phones"} />
-      <Carousel data={phones} />
+      {phones && <Carousel data={phones} />}
       <Separator />
       <Heading body={"Laptops"} />
-      <Carousel data={laptops} />
+      {laptops && <Carousel data={laptops} />}
       <Separator />
       <Heading body={"TVs"} />
-      <Carousel data={tvs} />
+      {tvs && <Carousel data={tvs} />}
       <Footer />
     </div>
   );
