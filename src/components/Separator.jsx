0631a2be6e52399f50462867e559bdc07bd6ed18
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.comnode>
2023-12-02 22:26:29 +0530

    ensured styles

--- src/components/Separator.jsx
+++ src/components/Separator.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Separator = () => {
   const style = {
-    borderTop: "0 !important",
-    borderLeft: "0 !important",
-    borderRight: "0 !important",
-    borderBottom: "12px dotted lightgray !important",
-    width: "50px !important",
-    margin: "20px auto !important"
+    borderTop: "0",
+    borderLeft: "0",
+    borderRight: "0",
+    borderBottom: "12px dotted lightgray",
+    width: "50px",
+    margin: "20px auto"
   }
   return (
     <hr style={style}/>
